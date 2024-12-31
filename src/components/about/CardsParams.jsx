cf25b34c72a23030aac58bc452419009480c5700
ritu-477 <ritujoharbishnoi@gmail.com>
2024-12-31 12:24:02 +0530

    update-live link

--- src/components/about/CardsParams.jsx
+++ src/components/about/CardsParams.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import { useSearchParams } from 'react-router';
 import { CARD_DATA } from '../../utils/helper';
+import Heading from '../../common/Heading';
 
-const Cards = () => {
+const CardsParams = () => {
     const [searchParams, setSearchParams] = useSearchParams();
 
     const handleClick = (card) => {
         setSearchParams({ card: card.replace(/ /g, '-') });
     };
 
-    const selectedCard = searchParams.get('card');
+    const cardSelected = searchParams.get('card');
 
     return (
         <div className='bg-dark-blue'>
             <div className="container row row-gap-4 mx-auto py-5">
-                <h2 className='text-white text-center'>Card Params</h2>
-                {CARD_DATA.map((card, i) => (
-                    <div className="col-lg-4 col-md-6 col-sm-12" key={i}>
+                <Heading classStyle={'text-white text-center'} text={'Cards Params'} />
+                {CARD_DATA.map((card, index) => (
+                    <div className="col-lg-4 col-md-6 col-sm-12" key={index}>
                         <div
                             onClick={() => handleClick(card.heading)}
-                            className={`card px-2 ${selectedCard === card.heading.replace(/ /g, '-') ? 'bg-dark-green' : ''}`}>
-                            <h2 className={`text-center cursor-pointer ${selectedCard === card.heading.replace(/ /g, '-') ? 'text-white' : ''}`} >
+                            className={`card px-2 ${cardSelected === card.heading.replace(/ /g, '-') ? 'bg-dark-green' : ''}`}>
+                            <h3 className={`text-center cursor-pointer ${cardSelected === card.heading.replace(/ /g, '-') ? 'text-white' : ''}`} >
                                 {card.heading}
-                            </h2>
-                            <p className={`text-center cursor-pointer ${selectedCard === card.heading.replace(/ /g, '-') ? 'text-white' : ''}`}>
+                            </h3>
+                            <p className={`text-center cursor-pointer ${cardSelected === card.heading.replace(/ /g, '-') ? 'text-white' : ''}`}>
                                 {card.description}
                             </p>
                         </div >
@@ -34,4 +35,4 @@ const Cards = () => {
     );
 };
 
-export default Cards;
+export default CardsParams;
